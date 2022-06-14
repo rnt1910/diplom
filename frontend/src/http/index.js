05b4dbe1cf4ebc04f002f75bd7175b2ceac20e80
Alexander Blinov <spichkens.2002@mail.ru>
2022-06-14 18:11:47 +0300

    .

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const API_URL = `http://192.168.1.4:5000/`
+export const API_URL = `http://ozon-learning.ru/`
 
 const $host = axios.create({
-    baseURL: API_URL
+	baseURL: API_URL,
 })
 
 const $authHost = axios.create({
-    baseURL: API_URL
+	baseURL: API_URL,
 })
 
-const authInterceptor = async (config) => {
-    const token = await localStorage.getItem('token')
-    config.headers.authorization = `Bearer ${token}`
-    return config;
+const authInterceptor = async config => {
+	const token = await localStorage.getItem('token')
+	config.headers.authorization = `Bearer ${token}`
+	return config
 }
 
 $authHost.interceptors.request.use(authInterceptor)
